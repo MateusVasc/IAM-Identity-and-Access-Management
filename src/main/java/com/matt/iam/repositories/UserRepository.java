f509095e9ae95b0ab7f34386713d436c8ab05adf
MateusVasc <mvaraujo2004@gmail.com>
2025-05-02 23:32:31 -0300

    feat: adds user interface

---
+++ src/main/java/com/matt/iam/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.matt.iam.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matt.iam.entities.User;
+
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
