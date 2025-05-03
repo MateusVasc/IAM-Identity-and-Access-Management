bcabbb44a60467c9e259fb74b2ec95b6e0b7ec5b
MateusVasc <mvaraujo2004@gmail.com>
2025-05-02 22:43:23 -0300

    feat: adds permission entity

---
+++ src/main/java/com/matt/iam/entities/Permission.java
@@ -0,0 +1,29 @@
+package com.matt.iam.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "permissions")
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
