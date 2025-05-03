4b9e8a949e73fd847ff0cc6a1154e5fbbd6d8d76
MateusVasc <mvaraujo2004@gmail.com>
2025-05-02 22:42:53 -0300

    feat: adds user entity

--- src/main/java/com/matt/iam/entities/User.java
+++ src/main/java/com/matt/iam/entities/User.java
@@ -1,12 +1,20 @@
 package com.matt.iam.entities;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -32,4 +40,15 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @ManyToAny
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<RefreshToken> tokens = new HashSet<>();
 }
