21e06e1529cb3bf91d1e0bf198f4ab195eb3ee0f
MateusVasc <mvaraujo2004@gmail.com>
2025-05-04 18:21:30 -0300

    feat: adds role repository

--- src/main/java/com/matt/iam/repositories/UserRepository.java
+++ src/main/java/com/matt/iam/repositories/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.matt.iam.entities.User;
 
-
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 }
