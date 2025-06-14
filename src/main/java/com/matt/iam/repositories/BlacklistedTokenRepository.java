624b27dd9ebad649cd48547d3ba5b12d0c2092ed
MateusVasc <mvaraujo2004@gmail.com>
2025-06-14 16:20:26 -0300

    feat: adds optimazed queries

--- src/main/java/com/matt/iam/repositories/BlacklistedTokenRepository.java
+++ src/main/java/com/matt/iam/repositories/BlacklistedTokenRepository.java
@@ -1,11 +1,21 @@
 package com.matt.iam.repositories;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.matt.iam.entities.BlacklistedToken;
 
 public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, UUID> {
     boolean existsByToken(String token);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM BlacklistedToken b WHERE b.expiresAt < :currentTime")
+    void deleteExpiredTokens(@Param("currentTime") LocalDateTime currentTime);
 }
--- src/main/java/com/matt/iam/repositories/UserRepository.java
+++ src/main/java/com/matt/iam/repositories/UserRepository.java
@@ -3,10 +3,20 @@ package com.matt.iam.repositories;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.matt.iam.entities.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+
+    @EntityGraph(attributePaths = {"roles", "roles.permissions"})
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmailWithRolesAndPermissions(@Param("email") String email);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmailBasic(@Param("email") String email);
 }
